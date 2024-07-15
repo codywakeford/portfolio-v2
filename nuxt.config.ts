cbf4d3e760ac3fbee7c19d48b13738c9d6ca7f30
cwake3218 <cody@gourmai.co.uk>
2024-07-15 13:03:11 +0100

    bug fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: false },
-    extends: ["../nova-markup"],
+    extends: ["github:codywakeford/nova-markup#version-1.1.0"],
     modules: ["@nuxt/fonts", "@nuxt/content", "@nuxt/image"],
 
     vite: {
